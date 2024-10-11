5810412d2c4c8e3e19dc4e30ee336e6934c2fb6f
KEIVISSON CAMPOS <keivisson.dev@gmail.com>
2024-10-11 09:32:34 -0300

    Atualizado para baixar img SVG

--- js/script.js
+++ js/script.js
@@ -1,38 +1,41 @@
 const container = document.querySelector(".container");
 const qrcodeInput = document.querySelector("#qr-code input");
 const geradorButton = document.querySelector("#qr-code button");
-const qrcodeImg = document.querySelector("#img-qrcode img");
+const qrcodeImg = document.querySelector("#img-qrcode");
+const downloadLink = document.querySelector("#download-link");
 
-function geradorQrCode(){
-    const qrcodeInputValue = qrcodeInput.value;  
+function geradorQrCode() {
+    const qrcodeInputValue = qrcodeInput.value;
 
-    if(!qrcodeInputValue) return;
-    
+    if (!qrcodeInputValue) return;
+
     geradorButton.innerText = "Gerando código...";
 
-    console.log(qrcodeImg);
+    // Gera QR code em SVG
+    const svgURL = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrcodeInputValue}&format=svg`;
+    
+    // Atribui a URL ao src da imagem e ao href para download
+    qrcodeImg.src = svgURL;
+    downloadLink.href = svgURL;
 
-    qrcodeImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrcodeInput.value}`;
-
-    qrcodeImg.addEventListener("load", () =>{
+    qrcodeImg.addEventListener("load", () => {
         container.classList.add("active");
         geradorButton.innerText = "Gerar QR Code";
-    })
-    
+    });
 }
 
 geradorButton.addEventListener("click", () => {
     geradorQrCode();
-})
+});
 
 qrcodeInput.addEventListener("keydown", (e) => {
-    if(e.code === "Enter"){
+    if (e.code === "Enter") {
         geradorQrCode();
     }
 });
 
 qrcodeInput.addEventListener("keyup", (e) => {
-    if(!qrcodeInput.value){
+    if (!qrcodeInput.value) {
         container.classList.remove("active");
         geradorButton.innerText = "Gerar QR Code";
     }
